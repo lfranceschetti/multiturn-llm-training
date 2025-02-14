f70b76d73ea742d31a578a6d3775bff008b4f45a
lfranceschetti <lfranceschetti@gmx.ch>
2025-02-14 16:39:08 +0100

    Big Logger update

--- grpo.sh
+++ grpo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=3:59:00          # Max runtime
+#SBATCH --time=23:59:00          # Max runtime
 #SBATCH --mem-per-cpu=50G         # Memory per CPU
 #SBATCH --nodes=1                # Number of nodes
 #SBATCH --gpus=a100_80gb:2
 #SBATCH --cpus-per-task=2
 #SBATCH --ntasks=2
-#SBATCH --output=grpo.out    # Output log file
-#SBATCH --error=grpo.err    # Error log file
+#SBATCH --output=grpo_online.out    # Output log file
+#SBATCH --error=grpo_online.err    # Error log file
 
 
 # Load necessary modules
@@ -23,5 +23,5 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 # Execute Python script
-accelerate launch --config_file=/cluster/home/fraluca/.cache/huggingface/accelerate/default_config.yaml --main_process_port=29502 train.py --config-name 8B_GRPO_2
+accelerate launch --config_file=/cluster/home/fraluca/.cache/huggingface/accelerate/default_config.yaml --main_process_port=29501 train.py --config-name 8B_GRPO_1
  
